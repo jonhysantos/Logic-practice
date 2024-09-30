663ee673043b59d93868faad5c4870661b552907
Jonhy Santos <jonhy1905@hotmail.com>
2024-09-30 16:04:43 -0300

    pratica do dia.

---
+++ Logic-practice01/currency-convert-01/script01.js
@@ -0,0 +1,41 @@
+const convertCurrency = document.querySelector('.convert-currency');
+const currencySelect = document.querySelector('.currency-select');
+convertCurrency.addEventListener('click',convertValues);
+
+function convertValues(){
+    const inputCurrencyValue = document.querySelector('.input-currency').value
+    const currencyValueConvert = document.querySelector('.currency-value-convert');
+    const currencyValueConverted = document.querySelector('.currency-value-converted');
+
+    const dolarToday = 5.40
+    const euroToday = 6.10
+
+    if(currencySelect.value == 'dolar'){
+        currencyValueConverted.innerHTML = new Intl.NumberFormat('en-US',{style:'currency',currency:'USD'}).format(inputCurrencyValue / dolarToday);
+    }
+
+    if(currencySelect.value == 'euro'){
+        currencyValueConverted.innerHTML = new Intl.NumberFormat('de-DE',{style:'currency',currency:'EUR'}).format(inputCurrencyValue / euroToday)
+    }
+
+    currencyValueConvert.innerHTML = new Intl.NumberFormat('pt-BR',{style:'currency',currency:'BRL'}).format(inputCurrencyValue)
+}
+
+currencySelect.addEventListener('change',changeCurrency);
+
+function changeCurrency(){
+    const currencyName = document.getElementById('currency-name');
+    const currencyimage = document.querySelector('.currency-img');
+
+    if(currencySelect.value == 'dolar'){
+        currencyName.innerHTML = 'Dólar Americano'
+        currencyimage.src = './assets/img/currency-eua-logo.png'
+    }
+
+    if(currencySelect.value == 'euro'){
+        currencyName.innerHTML = 'Euro'
+        currencyimage.src = './assets/img/EUR-logo.png'
+    }
+
+    convertValues();
+}
---
+++ Logic-practice02/Search-for-of-00/script.js
@@ -0,0 +1,18 @@
+/*
+    For of
+    -O for of pepa cada letra ou cada posição do meu array.
+    
+
+*/
+
+const myName = 'Jonhy'
+const users = ['Jonhy','Priscila','Aninha','Stefany']
+
+for(const letter of myName){
+    console.log(letter)
+}
+
+for(const letter of users){
+    console.log(users)
+    
+}
