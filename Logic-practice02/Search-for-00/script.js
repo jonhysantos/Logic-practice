4edbf20f72402e0d19587a8164135b37d33d4d57
Jonhy Santos <jonhy1905@hotmail.com>
2024-10-02 22:15:10 -0300

    pratica de logica.

---
+++ Logic-practice02/Search-for-00/script.js
@@ -0,0 +1,6 @@
+// Sim! porque com educação de qualidade podemos fazer com que todos tenham acesso ao conhecimento e buscá-lo através da leitura, em diferentes meios.
+
+
+// Porque tais expressões. não podem ser usada em uma entrevista,que visa temas sérios.
+
+// Ajuda as mulheres com educação de qualidade, e incentivando elas a serem empderadas, com mais igualdade.
