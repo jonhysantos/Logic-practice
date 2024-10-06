22d9be72c78b1c5038e02b3a0be309071449412c
Jonhy Santos <jonhy1905@hotmail.com>
2024-10-06 18:30:26 -0300

    pratica do dia.

---
+++ Logic-practice01/Random-01/script05.js
@@ -0,0 +1,11 @@
+const buttonRandom = document.querySelector('button');
+buttonRandom.addEventListener('click',function(){
+    const min = Math.ceil(document.querySelector('.input-min').value);
+    const max = Math.floor(document.querySelector('.input-max').value);
+    if(min >= max){
+        alert(`O valor minimo deve ser menor que o valor máximo.`);
+    }else{
+        const result = Math.floor(Math.random() * (max -min) + min)
+        alert(result)
+    }
+})
