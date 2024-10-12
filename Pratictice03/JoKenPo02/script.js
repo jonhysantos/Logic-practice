0c48af3dbadba96be9792759e77af22532aaa0a0
Jonhy Santos <jonhy1905@hotmail.com>
2024-10-12 01:25:09 -0300

    pratica da madrugada.

--- Pratictice03/JoKenPo02/script.js
+++ Pratictice03/JoKenPo02/script.js
@@ -1,6 +1,25 @@
-
-
+const result = document.querySelector('.result')
 
 const playHuman = (humanChoice) => {
-    console.log(humanChoice)
+    playTheGame(humanChoice, playMachine())
+    
+}
+
+const playMachine = () => {
+    const choice = ['rock', 'paper', 'scissors']
+    const randomNumber = Math.floor(Math.random() * 3)
+    return choice[randomNumber]
+}
+
+const playTheGame = (human, machine) => {
+    if (human === machine) {
+        result.innerHTML = 'Deu empate!'
+    } else if ((human === 'rock' && machine === 'scissors') ||
+        (human === 'paper' && machine === 'rock') ||
+        (human === 'scissors' && machine === 'paper')) {
+        result.innerHTML = 'Você Ganhou!'
+    }else {
+        result.innerHTML = 'Você perdeu para a Alexa'
+    }
+    console.log(human,machine)
 }
