a841472f4067ce9be19090440f4c1a6a12cc5bfa
Jonhy Santos <jonhy1905@hotmail.com>
2024-10-13 01:23:41 -0300

    Consegui fazer sozinho.

--- Pratictice03/JoKenPo02/script.js
+++ Pratictice03/JoKenPo02/script.js
@@ -1,25 +1,36 @@
-const result = document.querySelector('.result')
+const result = document.querySelector('.result');
+const humanScore = document.querySelector('#human-score');
+const machineScore = document.querySelector('#machine-score');
+
 
 const playHuman = (humanChoice) => {
-    playTheGame(humanChoice, playMachine())
+    playTheGame(humanChoice,playMachine())
     
 }
 
 const playMachine = () => {
-    const choice = ['rock', 'paper', 'scissors']
+    const choice = ['rock','paper','scissors']
     const randomNumber = Math.floor(Math.random() * 3)
     return choice[randomNumber]
 }
 
-const playTheGame = (human, machine) => {
-    if (human === machine) {
+let pointsUsu = 0;
+let pointsAlex = 0
+
+const playTheGame = (human,machine) => {
+    console.log(human,machine)
+    if(human === machine){
         result.innerHTML = 'Deu empate!'
-    } else if ((human === 'rock' && machine === 'scissors') ||
+    }else if(human === 'rock' && machine === 'scissors' || 
         (human === 'paper' && machine === 'rock') ||
-        (human === 'scissors' && machine === 'paper')) {
-        result.innerHTML = 'Você Ganhou!'
+    (human === 'scissors' && machine === 'paper')){
+        
+        result.innerHTML = `Você ganhou!`
+        pointsUsu ++
+        humanScore.innerHTML =  pointsUsu
     }else {
         result.innerHTML = 'Você perdeu para a Alexa'
+        pointsAlex ++
+        machineScore.innerHTML = pointsAlex
     }
-    console.log(human,machine)
 }
