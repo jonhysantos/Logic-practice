faf8e85d25f39cb66c2a5e45d048112f3f7d3849
Jonhy Santos <jonhy1905@hotmail.com>
2024-10-28 22:27:38 -0300

    pratica do dia .

--- Pratictice03/JoKenPo11/script.js
+++ Pratictice03/JoKenPo11/script.js
@@ -1,10 +1,10 @@
-const resutl = document.querySelector('.result');
+const result = document.querySelector('.result');
 const humanScore = document.querySelector('#human-score');
 const machineScore = document.querySelector('#machine-score');
 
-let pointsMachine = 0;
-let pointshuman = 0;
 
+let pointsH = 0;
+let pointsM = 0;
 
 
 const GAME_OPTIONS = {
@@ -15,30 +15,30 @@ const GAME_OPTIONS = {
 
 
 const playHuman = (humanChoice) => {
-    playTheGame(humanChoice,playMachine())
+    playTheGame(humanChoice, playMachine())
 }
 
 const playMachine = () => {
     const choice = [GAME_OPTIONS.ROCK,GAME_OPTIONS.PAPER,GAME_OPTIONS.SCISSORS]
-    const NumberRandom = Math.floor(Math.random () * 3)
-    return choice[NumberRandom]
+    const randomNumber = Math.floor(Math.random() * 3)
+    return choice[randomNumber]
 }
 
 const playTheGame = (human,machine) => {
     console.log(human,machine)
     if(human === machine){
-        resutl.innerHTML = 'Deu empate!'
-    }else if (human === GAME_OPTIONS.ROCK && machine === GAME_OPTIONS.SCISSORS ||
+        result.innerHTML = 'Deu empate!'
+    }else if(human === GAME_OPTIONS.ROCK && machine === GAME_OPTIONS.SCISSORS ||
         (human === GAME_OPTIONS.PAPER && machine === GAME_OPTIONS.ROCK ||
             (human === GAME_OPTIONS.SCISSORS && machine === GAME_OPTIONS.PAPER)
         )
     ){
-        pointshuman ++
-        humanScore.innerHTML = pointshuman
-        resutl.innerHTML = 'Você ganhou!'
+        pointsH ++
+        humanScore.innerHTML = pointsH
+        result.innerHTML = 'Você ganhou!'
     }else {
-        pointsMachine ++
-        machineScore.innerHTML = pointsMachine
-        resutl.innerHTML = 'Você perdeu para a Alexa'
+        pointsM ++
+        machineScore.innerHTML = pointsM
+        result.innerHTML = 'Você perdeu para a Alexa'
     }
 }
