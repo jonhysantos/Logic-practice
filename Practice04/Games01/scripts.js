6846f9cacd2cfaad9388821c32dc9717fb5f4939
Jonhy Santos <jonhy1905@hotmail.com>
2024-11-25 17:28:24 -0300

    pratica do dia.

--- Practice04/Games01/scripts.js
+++ Practice04/Games01/scripts.js
@@ -2,17 +2,21 @@ import {games} from './products.js'
 
 const btnShowAll = document.querySelector('.show-all');
 const list = document.querySelector('ul');
-const btnMapAll = document.querySelector('.map-all');
+const btnmapAll = document.querySelector('.map-all')
+const btnreduceAll = document.querySelector('.sum-all');
+const btnFilterAll = document.querySelector('.filter-all');
 
 function currencyFormat(value){
     const newValue = value.toLocaleString('pt-BR',{style:'currency',currency:'BRL'})
     return newValue
 }
 
+
+
 function showAll(arrayProduct){
-    let myli = ''
+    let myLi = ''
     arrayProduct.forEach(product => {
-        myli += `
+        myLi += `
             <li>
                 <img src=${product.src}>
                 <p>${product.name}</p>
@@ -20,12 +24,29 @@ function showAll(arrayProduct){
             </li>
         `
     })
-    list.innerHTML = myli
+    list.innerHTML = myLi
 }
 
 function mapAll(){
+    const newPrices = games.map((product => ({
+        ...product,
+        price:product.price * 0.9
+    })))
+    showAll(newPrices)
+}
+
+function reduceAll(){
+    const total = games.reduce((acc,curr) => acc + curr.price,0)
+    list.innerHTML = `O valor total dos itens é de ${total}`
 
 }
 
-btnShowAll.addEventListener('click',() => showAll(games))
-btnMapAll.addEventListener('click',mapAll)
+function filterAll(){
+    const justHorror = games.filter(product => product.genero === 'Survivor horror')
+    showAll(justHorror)
+}
+
+btnShowAll.addEventListener('click',() => showAll(games));
+btnmapAll.addEventListener('click',mapAll)
+btnreduceAll.addEventListener('click',reduceAll)
+btnFilterAll.addEventListener('click',filterAll)
