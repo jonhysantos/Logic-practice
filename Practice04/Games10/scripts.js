8110a1a82863cbfcd4e6f035e336823c99b55521
Jonhy Santos <jonhy1905@hotmail.com>
2024-12-19 22:05:48 -0300

    pratica do dia.

--- Practice04/Games10/scripts.js
+++ Practice04/Games10/scripts.js
@@ -2,6 +2,12 @@ import {games} from './products.js'
 
 const btnShowAll = document.querySelector('.show-all');
 const list = document.querySelector('ul');
+const btnMapAll = document.querySelector('.map-all');
+
+function currencyFormat(value){
+    const real = value.toLocaleString('pt-bt',{style:'currency',currency:'BRL'})
+    return real
+}
 
 function showAll(arrayProduct){
     let myLi = ''
@@ -10,11 +16,21 @@ function showAll(arrayProduct){
             <li>
                 <img src=${product.src}>
                 <p>${product.name}</p>
-                <p class="item-price">${product.price}</p>
+                <p class="item-price">${currencyFormat(product.price)}</p>
             </li>
         `
     })
     list.innerHTML = myLi
 }
 
+function mapAll(){
+    const newPrices = games.map((product => ({
+        ...product,
+        price:product.price * 0.43
+    })))
+
+    showAll(newPrices)
+}
+
 btnShowAll.addEventListener('click',() => showAll(games))
+btnMapAll.addEventListener('click',mapAll)
