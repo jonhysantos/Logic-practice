e82d4b820465d07d1b9d2e415f62b310a40034b8
Jonhy Santos <jonhy1905@hotmail.com>
2024-12-27 14:03:06 -0300

    pratica do dia

--- Practice04/Games12/scripts.js
+++ Practice04/Games12/scripts.js
@@ -2,6 +2,9 @@ import {games} from './products.js'
 
 const btnShowAll = document.querySelector('.show-all');
 const list = document.querySelector('ul');
+const btnMapAll = document.querySelector('.map-all');
+const btnSumAll = document.querySelector('.sum-all');
+const btnFilterAll = document.querySelector('.filter-all');
 
 function formatCurrency(value){
     const real = value.toLocaleString('pt-BR',{style:'currency',currency:'BRL'})
@@ -22,4 +25,26 @@ function showAll(arrayProduct){
     list.innerHTML = myLi
 }
 
+function mapAll(){
+    const newPrice = games.map(product => ({
+        ...product,
+        price:product.price * 0.43
+    }))
+    showAll(newPrice)
+}
+
+function sumAll(){
+    const total = games.reduce((acc,curr) => acc + curr.price,0)
+    list.innerHTML = `O total de todos os tens é de ${formatCurrency(total)}`
+    list.style.fontSize = '2em'
+}
+
+function filterAll(){
+    const justHorror = games.filter(product => product.genero === 'Survivor horror')
+    showAll(justHorror)
+}
+
 btnShowAll.addEventListener('click',() => showAll(games));
+btnMapAll.addEventListener('click',mapAll);
+btnSumAll.addEventListener('click',sumAll)
+btnFilterAll.addEventListener('click',filterAll)
