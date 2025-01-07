851053043c466960c416e717b3559b49d22a5241
Jonhy Santos <jonhy1905@hotmail.com>
2025-01-07 14:15:13 -0300

    pratica do dia.

--- Practice04/Games13/scripts.js
+++ Practice04/Games13/scripts.js
@@ -1,5 +1,20 @@
 import {games} from './products.js'
 
 const btnAllShow = document.querySelector('.show-all');
+const list = document.querySelector('ul');
 
-function showAll
+function showAll(arrayProduct){
+    let myLi = ""
+    arrayProduct.forEach(product => {
+        myLi += `
+            <li>
+                <img src=${product.src}>
+                <p>${product.name}</p>
+                <p class="item-price">${product.price}</p>
+            </li>
+        `
+    })
+    list.innerHTML = myLi
+}
+
+btnAllShow.addEventListener('click',() => showAll(games))
